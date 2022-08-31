71157ede4ed21af190df05aaa683bd193a2822ee
pythjango <malutheertha@481gmail.com>
2022-08-30 20:34:01 -0700

    first commit

---
+++ djangoo/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+def djangoo_master(request):
+    return render (request,'frame/master.html')
+
+def djangoo_home(request):
+    return render (request,'frame/home.html')
+
+def djangoo_about(request):
+    return render (request,'frame/about.html')
+
+def djangoo_contact(request):
+    return render (request,'frame/contact.html')
+
+def djangoo_css(request):
+    return render (request,'frame/css.html')
+
+    
+
+       
+
+# Create your views here.
